e4af77191ccfc34e6bf1a2b9ecdbf128691545f4
jorgin <jorgeigor123@hotmail.com>
2025-03-12 08:23:50 -0300

    Completed beta design, beginning JavaScript development

---
+++ script/script.js
@@ -0,0 +1,14 @@
+const form = document.querySelector("form")
+
+form.onsubmit = (event) => {
+    event.preventDefault
+    function executar(){
+        const objeto = {
+            date: "",
+            title: "",
+            value: "",
+            entry: "",
+
+        }
+    }
+}
