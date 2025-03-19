a6941261a6385711473919f3b9e04f3bdadb37ad
jorgin <jorgeigor123@hotmail.com>
2025-03-19 07:56:27 -0300

    implementing functionality for the expense, income and total columns

--- script/script.js
+++ script/script.js
@@ -1,16 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
     const forms = document.querySelectorAll("form");
-
+    let somaRenda = 0
+    let somaGasto = 0
+    let somaTotal = 0
     forms.forEach((form) => {
         form.addEventListener("submit", (event) => {
             event.preventDefault();
-
+            
             function executar() {
                 const inputDate = document.getElementById("dt");
                 const inputTitle = document.getElementById("title")
                 const inputValue = document.getElementById("value")
                 const inputEntry = document.getElementById("entry")
-
+                let rendaMensal = document.getElementById("rendaMensal")
+                const gastoMensal = document.getElementById("gastoMensal")
+                let totalRenda = document.getElementById("totalRenda")
                 const object = {
                     date: inputDate.value,
                     title: inputTitle.value,
@@ -25,9 +29,14 @@ document.addEventListener("DOMContentLoaded", () => {
                 const entry = document.createElement("span");
                 const value = document.createElement("span");
                 const trashButton = document.createElement("button")
+                const trashImg = document.createElement("img")
                 
+                
+
                 // Adicionar a classe à <div>
                 new_element.className = "teste";
+                trashButton.className = "removeButton"
+
                 switch(inputEntry.value){
                     case "Renda mensal":
                         entry.className = "entry"
@@ -40,19 +49,44 @@ document.addEventListener("DOMContentLoaded", () => {
                 title.textContent = `${object.title}`
                 entry.textContent = `${object.entry}`
                 value.textContent = `R$${object.value}`
-
                 // Adicionar o <span> dentro da <div>
                 new_element.appendChild(text);
                 new_element.appendChild(title)
                 new_element.appendChild(entry)
                 new_element.appendChild(value)
+                trashButton.appendChild(trashImg)
+                new_element.appendChild(trashButton)
+                
                 // Adicionar a <div> ao <body> ou a outro elemento da página
                 let element = document.querySelector(".contentBox")
                 element.appendChild(new_element)
+
+                trashButton.addEventListener("click", (event) => {
+                    event.preventDefault()
+                    new_element.className = "remove"
+                })
+
+                if(inputEntry.value == "Renda mensal"){
+                    somaRenda += Number(inputValue.value)
+                    rendaMensal.innerHTML = 'R$' + String(somaRenda)
+                    rendaMensal.className = "texto"
+                }
+                else if(inputEntry.value == "Gasto mensal"){
+                    somaGasto += Number(inputValue.value)
+                    gastoMensal.innerHTML = 'R$' + String(somaGasto)
+                    gastoMensal.className = "texto"
+                }
+                if(totalRenda.innerHTML == "R$0"){
+                    somaTotal += somaRenda - somaGasto
+                    totalRenda.innerHTML = 'R$' + String(somaTotal)
+                    totalRenda.className = "texto"
+                    
+                }
             }
 
             executar();
             
         });
+        
     });
 });
